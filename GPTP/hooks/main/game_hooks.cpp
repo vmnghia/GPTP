258fe1c9a2ef7fcfb45cedadc01be15d91b634a3
vmnghia <nghiavm187@gmail.com>
2025-05-20 23:27:04 +0700

    chore: format game_hooks.cpp

--- GPTP/hooks/main/game_hooks.cpp
+++ GPTP/hooks/main/game_hooks.cpp
@@ -4,143 +4,137 @@
 
 #include <graphics/graphics.h>
 
-#include <SCBW/api.h>
 #include <SCBW/UnitFinder.h>
+#include <SCBW/api.h>
 
 #include "../psi_field.h"
 
 #include <cstdio>
 
 namespace utils {
-    void loopThroughVisibleUnits(void (*callback)(CUnit*)) {
-        //Loop through all units in the game.
-        for (CUnit* unit = *firstVisibleUnit; unit; unit = unit->link.next) {
+	void loopThroughVisibleUnits(void (*callback)(CUnit *)) {
+		// Loop through all units in the game.
+		for (CUnit *unit = *firstVisibleUnit; unit; unit = unit->link.next) {
 			callback(unit);
-        }
+		}
 	}
 
-    bool isMineral(const CUnit* resource) {
-        return  resource->id >= UnitId::ResourceMineralField && resource->id <= UnitId::ResourceMineralFieldType3;
-    }
-}
+	bool isMineral(const CUnit *resource) {
+		return resource->id >= UnitId::ResourceMineralField && resource->id <= UnitId::ResourceMineralFieldType3;
+	}
+} // namespace utils
 
 namespace plugins {
-    class HarvestTargetFinder : public scbw::UnitFinderCallbackMatchInterface {
-        CUnit* mainHarvester;
-    public:
-        void setMainHarvester(CUnit* mainHarvester) { this->mainHarvester = mainHarvester; }
-        bool match(CUnit* unit) {
-            if (!unit)
-                return false;
+	class HarvestTargetFinder : public scbw::UnitFinderCallbackMatchInterface {
+		CUnit *mainHarvester;
 
-            if (mainHarvester->getDistanceToTarget(unit) > (16 << 5)) //Harvest distance
-                return false;
+	  public:
+		void setMainHarvester(CUnit *mainHarvester) {
+			this->mainHarvester = mainHarvester;
+		}
+		bool match(CUnit *unit) {
+			if (!unit)
+				return false;
 
-            if (!(utils::isMineral(unit)))
-                return false;
+			if (mainHarvester->getDistanceToTarget(unit) > (16 << 5)) // Harvest distance
+				return false;
 
-            return true;
-        }
-    };
-    HarvestTargetFinder harvestTargetFinder;
+			if (!(utils::isMineral(unit)))
+				return false;
 
-    void exploreMap() {
-        for (int x = 0; x < mapTileSize->width; x++) {
-            for (int y = 0; y < mapTileSize->height; y++) {
+			return true;
+		}
+	};
+	HarvestTargetFinder harvestTargetFinder;
 
-                if ((x + y) % 2) {
-                    ActiveTile* currentTile = &(*activeTileArray)[(x)+mapTileSize->width * (y)];
-                    currentTile->exploredFlags = 0;
+	void exploreMap() {
+		for (int x = 0; x < mapTileSize->width; x++) {
+			for (int y = 0; y < mapTileSize->height; y++) {
 
-                }
-
-            }
-        }
-        //By RavenWolf:
-        //if vespene geyser or a mineral field, reveal it to all players
-        for (CUnit* unit = *firstVisibleUnit; unit; unit = unit->link.next) {
-            if (utils::isMineral(unit)
-                || unit->id == UnitId::ResourceVespeneGeyser) {
-                unit->sprite->visibilityFlags = 0xFF;
-            }
-        }
-    }
-
-    void executeFirstFrameRoutine() {
-        if (*elapsedTimeFrames == 0) {
-            scbw::printText(PLUGIN_NAME ": Hello, world!");
-
-            if (*GAME_TYPE != GameType::UseMapSettings) {
-                exploreMap();
-                CUnit* firstMineral[8];
-                for (CUnit* base = *firstVisibleUnit; base; base = base->link.next) {
-                    if (units_dat::BaseProperty[base->id] & UnitProperty::ResourceDepot)
-                    {
-                        harvestTargetFinder.setMainHarvester(base);
-                        firstMineral[base->playerId] = scbw::UnitFinder::getNearestTarget(
-                            base->getX() - 512, base->getY() - 512,
-                            base->getX() + 512, base->getY() + 512,
-                            base,
-                            harvestTargetFinder);
-                    }
-                }
-
-                for (CUnit* worker = *firstVisibleUnit; worker; worker = worker->link.next) {
-                    //KYSXD Send all units to harvest on first run
-                    if (units_dat::BaseProperty[worker->id] & UnitProperty::Worker) {
-                        if (firstMineral[worker->playerId]) {
-                            worker->orderTo(OrderId::Harvest1, firstMineral[worker->playerId]);
-                        }
-                    }
-                }
-            }
-        }
+				if ((x + y) % 2) {
+					ActiveTile *currentTile = &(*activeTileArray)[(x) + mapTileSize->width * (y)];
+					currentTile->exploredFlags = 0;
+				}
+			}
+		}
+		// By RavenWolf:
+		// if vespene geyser or a mineral field, reveal it to all players
+		for (CUnit *unit = *firstVisibleUnit; unit; unit = unit->link.next) {
+			if (utils::isMineral(unit) || unit->id == UnitId::ResourceVespeneGeyser) {
+				unit->sprite->visibilityFlags = 0xFF;
+			}
+		}
 	}
-}
+
+	void executeFirstFrameRoutine() {
+		if (*elapsedTimeFrames == 0) {
+			scbw::printText(PLUGIN_NAME ": Hello, world!");
+
+			if (*GAME_TYPE != GameType::UseMapSettings) {
+				exploreMap();
+				CUnit *firstMineral[8];
+				for (CUnit *base = *firstVisibleUnit; base; base = base->link.next) {
+					if (units_dat::BaseProperty[base->id] & UnitProperty::ResourceDepot) {
+						harvestTargetFinder.setMainHarvester(base);
+						firstMineral[base->playerId] = scbw::UnitFinder::getNearestTarget(
+						    base->getX() - 512, base->getY() - 512, base->getX() + 512, base->getY() + 512, base,
+						    harvestTargetFinder);
+					}
+				}
+
+				for (CUnit *worker = *firstVisibleUnit; worker; worker = worker->link.next) {
+					// KYSXD Send all units to harvest on first run
+					if (units_dat::BaseProperty[worker->id] & UnitProperty::Worker) {
+						if (firstMineral[worker->playerId]) {
+							worker->orderTo(OrderId::Harvest1, firstMineral[worker->playerId]);
+						}
+					}
+				}
+			}
+		}
+	}
+} // namespace plugins
 
 namespace hooks {
 
-    /// This hook is called every frame.
-    bool nextFrame() {
+	/// This hook is called every frame.
+	bool nextFrame() {
 
-        if (!scbw::isGamePaused()) { //If the game is not paused
+		if (!scbw::isGamePaused()) { // If the game is not paused
 
-            scbw::setInGameLoopState(true); //Needed for scbw::random() to work
-            graphics::resetAllGraphics();
-            hooks::updatePsiFieldProviders();
+			scbw::setInGameLoopState(true); // Needed for scbw::random() to work
+			graphics::resetAllGraphics();
+			hooks::updatePsiFieldProviders();
 			plugins::executeFirstFrameRoutine();
 
-            utils::loopThroughVisibleUnits([](CUnit* unit) {
-                switch (unit->id) {
-                case UnitId::TerranSCV:
-                    break;
+			utils::loopThroughVisibleUnits([](CUnit *unit) {
+				switch (unit->id) {
+					case UnitId::TerranSCV:
+						unit->id;
+						break;
 				}
-            });
+			});
 
-            scbw::setInGameLoopState(false);
+			scbw::setInGameLoopState(false);
+		}
 
-        }
+		return true;
+	}
 
-        return true;
+	;
 
-    }
+	bool gameOn() {
+		return true;
+	}
 
-    ;
+	;
 
-    bool gameOn() {
-        return true;
-    }
+	bool gameEnd() {
+		return true;
+	}
 
-    ;
+	;
 
-    bool gameEnd() {
-        return true;
-    }
+} // namespace hooks
 
-    ;
-
-} //hooks
-
-namespace plugins {
-
-}
+namespace plugins {}
